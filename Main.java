eb95c93d24c609168c3849dc512aa108599dbf3b
Elena <111504533+Ellena2022@users.noreply.github.com>
2022-08-21 21:33:25 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        int a,b,c;
+        a=10;
+        b=15;
+        c=20;
+        int sum=a+b+c;
+
+        System.out.println("Сумма "+sum);
+    }
+
+    }
+
+
